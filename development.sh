799870ef2ee5d5940ff3964ef94b27ebee2eab92
Yuan Gao <gaoyuankidult@gmail.com>
2018-01-02 17:17:41 +0100

    Updated the submodules in order to use deep reinforcement learning.

--- development.sh
+++ development.sh
@@ -12,5 +12,13 @@ tar zxfv .tar.gz -C /media/alex/22E67196E6716AC5/Materials/Research/BlueCoast/
 export PYTHONPATH=${PYTHONPATH}:/media/alex/22E67196E6716AC5/Materials/Research/BlueCoast/pynaoqi-python2.7-2.5.5.5-linux64/lib/python2.7/site-packages
 
 # install opencv
-conda install -c conda-forge opencv 
+conda install -c conda-forge opencv
+
+# install tensorforce and gym using
+cd tensorforce
+pip install -e .[tf_gpu]
+
+cd gym
+pip install -e '.[atari]'
+conda install libgcc
 
